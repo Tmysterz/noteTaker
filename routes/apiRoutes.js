f3d91361199c1dd73a72512d5e486a72c3f1bf9a
Terrence McCarthy <terrencemccarthysnkrs@gmail.com>
2023-11-16 23:57:03 -0600

    test title is now showing in the recent searches

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
+const fs = require('fs');
+const fileName = './db/db.json'
 
 router.get('/notes', (req,res) => {
-    res.json('success');
+    const data = fs.readFileSync(fileName, 'utf8');
+    const parsedData = JSON.parse(data);
+    res.status(200).json(parsedData);
 });
+
+router.post('/notes', (req, res) => {
+    res.json('success');
+})
 
 
 
