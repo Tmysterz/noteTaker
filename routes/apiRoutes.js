2478e3de97c383a783c1be6428dd25cfbb9f4aad
Terrence McCarthy <terrencemccarthysnkrs@gmail.com>
2023-11-21 18:23:21 -0600

    completed functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const fileName = './db/db.json'
 
 router.get('/notes', (req,res) => {
@@ -9,9 +10,16 @@ router.get('/notes', (req,res) => {
 });
 
 router.post('/notes', (req, res) => {
-    res.json('success');
+    const data = fs.readFileSync(fileName, 'utf8');
+    const parsedData = JSON.parse(data);
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    }
+    parsedData.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(parsedData));
+    res.status(200).json(parsedData);
 })
-
-
 
 module.exports = router;
